8cfee20e125e4844e67afecbcb041c4ff1dc5467
akadriu <1157536+akadriu@users.noreply.github.com>
2025-08-04 22:49:04 +0200

    Update the way timing is shown

--- src/utils/timeUtils.js
+++ src/utils/timeUtils.js
@@ -1,12 +1,15 @@
 export const timeDifference = (fetchDate) => {
     const now = new Date();
     const fetchTime = new Date(fetchDate);
-    const diffInMinutes = Math.floor((now - fetchTime) / (1000 * 60));
+    const diffInMinutes = Math.floor((now - fetchTime) / (1000 * 60) - 120);
 
     if (diffInMinutes < 60) {
         return `${diffInMinutes} minuta`;
-    } else {
+    } else if (diffInMinutes < 24 * 60) {
         const diffInHours = Math.floor(diffInMinutes / 60);
         return `${diffInHours} ore`;
+    } else {
+        const diffInDays = Math.floor(diffInMinutes / (24 * 60));
+        return `${diffInDays} dite`;
     }
 };
