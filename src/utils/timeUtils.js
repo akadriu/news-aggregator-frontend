447ddabe5900ac3e9c2219fa53c6a98ab39a3add
akadriu <1157536+akadriu@users.noreply.github.com>
2025-09-27 20:19:42 +0200

    Change time util

--- src/utils/timeUtils.js
+++ src/utils/timeUtils.js
@@ -1,7 +1,7 @@
 export const timeDifference = (fetchDate) => {
     const now = new Date();
     const fetchTime = new Date(fetchDate);
-    const diffInMinutes = Math.floor((now - fetchTime) / (1000 * 60) - 120);
+    const diffInMinutes = Math.floor((now - fetchTime) / (1000 * 60) );
 
     if (diffInMinutes < 60) {
         return `${diffInMinutes} minutash`;
